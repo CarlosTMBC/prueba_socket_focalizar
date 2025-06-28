faf445250e3d74031102b4ff1f535b743f6c7d5f
CarlosCardaa <124748888+CarlosTMBC@users.noreply.github.com>
2025-06-28 06:51:17 +0000

    Carlos Cardona: se regreso y ahora se usa node 14

--- server/rutas/verificacionCaptchaRutas.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verificarCaptcha } = require('../../controladores/ctrCaptcha');
-
-router.post('/verificarCaptcha', verificarCaptcha);
-
-module.exports = router;
